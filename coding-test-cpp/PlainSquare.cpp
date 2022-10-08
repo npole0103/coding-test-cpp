b60f34eec7312bed9c422cc7ec57070994fd1fde
npole0103 <npole0103@naver.com>
2022-10-09 00:25:08 +0900

    22/10/09 LEVEL_2

---
+++ coding-test-cpp/PlainSquare.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//using namespace std;
+//
+//int gcd(int a, int b) {
+//    while (b) {
+//        int n = a % b;
+//        a = b;
+//        b = n;
+//    }
+//    return a;
+//}
+//
+//long long solution(int w, int h) {
+//    long long answer = (long long)w * (long long)h - (w + h) + gcd(w, h);
+//
+//    return answer;
+//}
+//
+//int main() {
+//
+//    cout << gcd(8, 12) << endl;
+//    return 0;
+//}
