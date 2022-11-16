d421a71ef7670870d5ffebaeb548c3c6229b36f6
npole0103 <npole0103@naver.com>
2022-11-17 00:00:30 +0900

    22/11/17 LEVEL_2

---
+++ coding-test-cpp/ExpectedMatchList.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//using namespace std;
+//
+//int solution(int n, int a, int b)
+//{
+//    int answer = 0;
+//
+//    while (true) {
+//        a = (a % 2 == 0 ? a / 2 : a / 2 + 1);
+//        b = (b % 2 == 0 ? b / 2 : b / 2 + 1);
+//        answer++;
+//        if (a == b) {
+//            break;
+//        }
+//    }
+//
+//    return answer;
+//}
+//
+//int main() {
+//
+//    return 0;
+//}
