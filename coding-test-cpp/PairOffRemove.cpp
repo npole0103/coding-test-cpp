d18ce8a6d4beed752a01bbfd68975eb85d58d59c
npole0103 <npole0103@naver.com>
2022-12-16 17:27:38 +0900

    22/12/16 LEVEL_2

---
+++ coding-test-cpp/PairOffRemove.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//using namespace std;
+//
+//stack<char> sc;
+//
+//int solution(string s)
+//{
+//    int answer = 1;
+//
+//    for (int i = 0; i < s.size(); i++) {
+//        if (!sc.empty() && sc.top() == s[i]) {
+//            sc.pop();
+//        }
+//        else {
+//            sc.push(s[i]);
+//        }
+//    }
+//
+//    return sc.empty() ? 1 : 0;
+//}
+//
+//int main() {
+//
+//    return 0;
+//}
