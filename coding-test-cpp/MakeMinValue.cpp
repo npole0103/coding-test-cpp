42b526b0639557cbb4f9a8b36c21572f507dbb86
npole0103 <npole0103@naver.com>
2023-01-18 02:31:10 +0900

    23/01/18 LEVEL_2

---
+++ coding-test-cpp/MakeMinValue.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//using namespace std;
+//
+//int solution(vector<int> A, vector<int> B)
+//{
+//    int acc = 0;
+//    int nSize = A.size();
+//
+//    sort(A.begin(), A.end());
+//    sort(B.begin(), B.end());
+//
+//    for (int i = 0; i < nSize; i++) {
+//        acc += A[i] * B[nSize - i - 1];
+//    }
+//
+//    return acc;
+//}
+//
+//int main() {
+//
+//    return 0;
+//}
