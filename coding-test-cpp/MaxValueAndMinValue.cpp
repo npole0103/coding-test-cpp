1f04b2a1e081148d96016ece41e0b49612709ef0
npole0103 <npole0103@naver.com>
2023-01-19 00:47:56 +0900

    23/01/19 LEVEL_2

---
+++ coding-test-cpp/MaxValueAndMinValue.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//#include <sstream>
+//using namespace std;
+//
+//string solution(string s) {
+//    string answer = "";
+//
+//    istringstream iss(s);
+//    int buf;
+//    vector<int> v;
+//
+//    while (iss >> buf) {
+//        v.push_back(buf);
+//    }
+//    
+//    sort(v.begin(), v.end());
+//
+//    answer += to_string(v[0]) + " " + to_string(v[v.size() - 1]);
+//
+//    return answer;
+//}
+//
+//int main() {
+//
+//    return 0;
+//}
