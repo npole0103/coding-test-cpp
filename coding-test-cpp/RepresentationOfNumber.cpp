7fc2c747715f8df5356e1fc940b091618e0f2994
npole0103 <npole0103@naver.com>
2023-01-23 13:01:04 +0900

    23/01/23 LEVEL_2

---
+++ coding-test-cpp/RepresentationOfNumber.cpp
@@ -0,0 +1,47 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//using namespace std;
+//
+//int solution(int n) {
+//    int answer = 0;
+//    
+//    int sum = n;
+//    int i = 1;
+//
+//    while (sum > 0) {
+//        if (sum % i == 0) {
+//            answer += 1;
+//        }
+//        sum -= i;
+//        i++;
+//    }
+//
+//    return answer;
+//}
+//
+//int main() {
+//
+//    /*
+//    
+//    15 / 1-> 15, 0
+//
+//    15 / 2 -> 7, 7, 1
+//
+//    15 / 3 -> 5 5 5, 0
+//
+//    15 / 4 -> 3 3 3 3, 3
+//
+//    15 / 5 -> 3 3 3 3 3, 0
+//    
+//    */
+//
+//
+//    return 0;
+//}
