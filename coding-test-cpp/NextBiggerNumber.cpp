1171f9b3670f76767db43a4f44681df802c7ace8
npole0103 <npole0103@naver.com>
2023-01-27 02:09:12 +0900

    23/01/27 LEVEL_2

---
+++ coding-test-cpp/NextBiggerNumber.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//using namespace std;
+//
+//int bitCount(int n) {
+//    int cnt = 0;
+//    while (n) {
+//        n = n & (n - 1);
+//        cnt++;
+//    }
+//    return cnt;
+//}
+//
+//int solution(int n) {
+//    int nBit = bitCount(n);
+//
+//    int i = n + 1;
+//    while (bitCount(i) != nBit) {
+//        i++;
+//    }
+//
+//    return i;
+//}
+//
+//int main() {
+//
+//    return 0;
+//}
