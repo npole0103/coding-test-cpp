efc58a5b3734f0f399f74f7fb770ad8dee6b8e51
npole0103 <npole0103@naver.com>
2023-01-30 00:40:51 +0900

    23/01/30 LEVEL_2

---
+++ coding-test-cpp/NumberOf124World.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//using namespace std;
+//
+//string decTo124World(int n) {
+//    string s;
+//    while (n > 0) {
+//        int remainder = n % 3;
+//        n /= 3;
+//
+//        if (remainder == 0) {
+//            s = "4" + s;
+//            n -= 1;
+//        }
+//        else {
+//            s = to_string(remainder) + s;
+//        }
+//    }
+//    
+//    return s;
+//}
+//
+//string solution(int n) {
+//
+//    return decTo124World(n);
+//}
+//
+//int main() {
+//
+//    /*
+//    5 12
+//    6 14
+//    7 21
+//    8 22
+//    9 24
+//    10 41
+//
+//    */
+//
+//    return 0;
+//}
