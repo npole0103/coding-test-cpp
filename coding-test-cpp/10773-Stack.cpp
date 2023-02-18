9fe07d93aedf09c2d458dd47f957f5462ebb6e90
npole0103 <npole0103@naver.com>
2023-02-18 23:24:38 +0900

    23/02/18 BOJ-Stack

---
+++ coding-test-cpp/10773-Stack.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//using namespace std;
+//
+//int main() {
+//    stack<int> s;
+//    int answer = 0;
+//
+//    int cnt;
+//    cin >> cnt;
+//
+//    int num;
+//    while (cnt--) {
+//        cin >> num;
+//        if (num != 0) {
+//            s.push(num);
+//        }
+//        else {
+//            s.pop();
+//        }
+//    }
+//
+//    while (!s.empty()) {
+//        answer += s.top();
+//        s.pop();
+//    }
+//
+//    cout << answer << endl;
+//
+//    return 0;
+//}
