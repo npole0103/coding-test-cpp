f813e10e1524211d183b71f7c4c528b8b2e99aa8
npole0103 <npole0103@naver.com>
2023-03-11 23:08:24 +0900

    23/03/11 BOJ-String

---
+++ coding-test-cpp/1316-String.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <math.h>
+using namespace std;
+
+bool isGroupWord(string str) {
+    set<char> s;
+
+    if (str.size() == 1) {
+        return true;
+    }
+
+    char prev = str[0];
+    s.insert(prev);
+
+    for (int i = 1; i < str.size(); i++) {
+        if (prev != str[i] && s.count(str[i]) != 0) {
+            return false;
+        }
+
+        prev = str[i];
+        s.insert(str[i]);
+    }
+
+    return true;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, answer = 0;
+    cin >> n;
+
+    vector<string> s(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+        answer += isGroupWord(s[i]) ? 1 : 0;
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
