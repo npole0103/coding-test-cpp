7ce4f5ebd5d0af20300df5e43c144535b721e360
npole0103 <npole0103@naver.com>
2023-03-15 17:56:46 +0900

    23/03/15 BOJ-Implementation

---
+++ coding-test-cpp/1193-Implementation.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//#include <set>
+//#include <map>
+//#include <queue>
+//#include <stack>
+//#include <math.h>
+//using namespace std;
+//
+//int main() {
+//
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+//
+//    int n;
+//    cin >> n;
+//
+//    int i = 1;
+//    while (n > i) {
+//        n -= i;
+//        i++;
+//    }
+//
+//    if (i % 2 == 0) {
+//        cout << n  << '/' << i + 1 - n  << endl;
+//    }
+//    else {
+//        cout << i + 1 - n << '/' << n << endl;
+//    }
+//
+//    return 0;
+//}
